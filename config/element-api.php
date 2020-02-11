d687248fb3e0b0ec22a13f17c66722be1a88e5fd
Christopher Kennedy <chris@onebrightlight.com>
2020-02-11 11:49:52 -0800

    Tweaked tabbing

--- config/element-api.php
+++ config/element-api.php
@@ -6,32 +6,32 @@ use craft\helpers\UrlHelper;
 $lastWeek = (new \DateTime('1 week ago'))->format(\DateTime::ATOM);
 
 return [
-    'endpoints' => [
-        'staff.json' => function() {
-            return [
-                'elementType' => Entry::class,
-                'criteria' => [
-                  'section' => 'staff',
-                  'status' => ['live', 'disabled'],
-                  'dateUpdated' => '>= {$lastWeek}'
-                ],
-                'transformer' => function(Entry $entry) {
-                    return [
-                        'title' => $entry->title,
-                        'status' => $entry->status,
-                        'id' => $entry->staffId,
-                        'email' => $entry->email,
-                        'givenName' => $entry->givenName,
-                        'middleName' => $entry->middleName,
-                        'surname' => $entry->surname,
-                        'phoneNumber' => $entry->phoneNumber,
-                        'jobTitle' => $entry->jobTitle,
-                        'department' => $entry->department,
-                        'organization' => $entry->organization,
-                        'staffSection' => $entry->staffSection
-                    ];
-                },
-            ];
+  'endpoints' => [
+    'staff.json' => function () {
+      return [
+        'elementType' => Entry::class,
+        'criteria' => [
+          'section' => 'staff',
+          'status' => ['live', 'disabled'],
+          'dateUpdated' => '>= {$lastWeek}'
+        ],
+        'transformer' => function (Entry $entry) {
+          return [
+            'title' => $entry->title,
+            'status' => $entry->status,
+            'id' => $entry->staffId,
+            'email' => $entry->email,
+            'givenName' => $entry->givenName,
+            'middleName' => $entry->middleName,
+            'surname' => $entry->surname,
+            'phoneNumber' => $entry->phoneNumber,
+            'jobTitle' => $entry->jobTitle,
+            'department' => $entry->department,
+            'organization' => $entry->organization,
+            'staffSection' => $entry->staffSection
+          ];
         },
-    ]
+      ];
+    },
+  ]
 ];
